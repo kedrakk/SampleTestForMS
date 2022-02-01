7d75747b486f65a26b73b4ae1fcb613d69ab0f88
kedrakk <keaindrak@gmail.com>
2022-02-01 20:19:54 +0630

    exercise 4

---
+++ SampleTestForMS/ReverseArr.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleTestForMS
+{
+    class ReverseArr
+    {
+        static int reverseResult(int num)
+        {
+            if (num.ToString().Length > 1)
+            {
+                String res = "";
+                if (num < 0)
+                {
+                    res = "-";
+                    num = Math.Abs(num);
+                }
+                for (int i = num.ToString().Length; i >= 1; i--)
+                {
+                    res += num.ToString()[i-1];
+                }
+                return int.Parse(res);
+            }
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = -12345;
+            Console.WriteLine("Result:" + reverseResult(num).ToString());
+        }
+    }
+}
